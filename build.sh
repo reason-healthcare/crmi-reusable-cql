c4f3d40e4a54b823e0de439719e0c9d21f105b46
Brian Kaney <brian@vermonster.com>
2024-08-21 11:34:59 -0400

    Update demonstration

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Building All IGs in parallel..." && \
-  (cd SubDependencyIG && ./_genonce.sh) & \
-  (cd DependencyIG && ./_genonce.sh) & \
-  (cd ParentIG && ./_genonce.sh) && wait
+  (cd SubDependencyIG && ./_genonce.sh) && \
+  (cd DependencyIG && ./_genonce.sh) && \
+  (cd ParentIG && ./_genonce.sh)
